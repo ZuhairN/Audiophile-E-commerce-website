1705f43cd35cc20af95a141eb9993990a97df4f3
ZuhairN <zuhairnawasreh@gmail.com>
2025-12-23 18:04:22 +0300

    update css reset file

--- components/boilerplate/Footer.js
+++ components/boilerplate/Footer.js
@@ -5,34 +5,38 @@ import NavList from './NavList';
 import logo from 'public/images/shared/desktop/logo.svg';
 
 export default function Footer() {
-    return (
-        <footer className='Footer'>
-            <div className='Footer__container'>
-                <nav className='Footer__nav'>
-                    <Link href='/' className='Footer__logo' legacyBehavior>
-                        <a aria-current='page'>
-                            <Image src={logo} alt='Audiophile E-commerce logo' />
-                        </a>
-                    </Link>
-                    <NavList />
-                </nav>
+  return (
+    <footer className='Footer'>
+      <div className='Footer__container'>
+        <nav className='Footer__nav'>
+          <Link href='/' className='Footer__logo' legacyBehavior>
+            <a aria-current='page'>
+              <Image src={logo} alt='Audiophile E-commerce logo' />
+            </a>
+          </Link>
+          <NavList />
+        </nav>
 
-                <div className='Footer__content'>
-                    <p className='para'>Audiophile is an all in one stop to fulfill your audio needs. We’re a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.</p>
-                    <p className='para--copyright'>Copyright 2025. All Rights Reserved</p>
-                    <div className='Footer__media'>
-                        <a href='https://twitter.com/' target='_blank' rel='noreferrer' aria-label='Visit our Twitter page'>
-                            <FaTwitter />
-                        </a>
-                        <a href='https://www.facebook.com/' target='_blank' rel='noreferrer' aria-label='Visit our Facebook page'>
-                            <FaFacebookSquare />
-                        </a>
-                        <a href='https://www.instagram.com/' target='_blank' rel='noreferrer' aria-label='Visit our Instagram page'>
-                            <FaInstagram />
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+        <div className='Footer__content'>
+          <p className='para'>
+            Audiophile is an all in one stop to fulfill your audio needs. We’re a small team of music lovers and sound
+            specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo
+            facility - we’re open 7 days a week.
+          </p>
+          <p className='para--copyright'>Copyright 2026. All Rights Reserved</p>
+          <div className='Footer__media'>
+            <a href='https://twitter.com/' target='_blank' rel='noreferrer' aria-label='Visit our Twitter page'>
+              <FaTwitter />
+            </a>
+            <a href='https://www.facebook.com/' target='_blank' rel='noreferrer' aria-label='Visit our Facebook page'>
+              <FaFacebookSquare />
+            </a>
+            <a href='https://www.instagram.com/' target='_blank' rel='noreferrer' aria-label='Visit our Instagram page'>
+              <FaInstagram />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- components/shared/Gear.js
+++ components/shared/Gear.js
@@ -1,20 +1,22 @@
 export default function Gear() {
-    return (
-        <div className='Gear'>
-            <picture className='Gear__img'>
-                <source media='(min-width: 53.5em)' srcSet='/images/shared/desktop/image-best-gear.jpg' />
-                <source media='(min-width: 33em)' srcSet='/images/shared/tablet/image-best-gear.jpg' />
-                <img src='/images/shared/mobile/image-best-gear.jpg' alt='Best-Gear' />
-            </picture>
-            <div className='Gear__content'>
-                <h2 className='heading--2'>
-                    BRINGING YOU THE <span>BEST</span> AUDIO GEAR
-                </h2>
-                <p className='para--dark'>
-                    Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable
-                    audio equipment.
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div className='Gear'>
+      <picture className='Gear__img'>
+        <source media='(min-width: 53.5em)' srcSet='/images/shared/desktop/image-best-gear.jpg' />
+        <source media='(min-width: 33em)' srcSet='/images/shared/tablet/image-best-gear.jpg' />
+        <img src='/images/shared/mobile/image-best-gear.jpg' alt='Best-Gear' />
+      </picture>
+      <div className='Gear__content'>
+        <h2 className='heading--2'>
+          BRINGING YOU THE <span>BEST</span> AUDIO GEAR
+        </h2>
+        <p className='para--dark'>
+          Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones,
+          speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to
+          browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who
+          make Audiophile the best place to buy your portable audio equipment.
+        </p>
+      </div>
+    </div>
+  );
 }
