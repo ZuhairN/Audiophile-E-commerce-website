6f106e43d386e624976fd3deb3c06dc16feed90e
ZuhairN <zuhairnawasreh@gmail.com>
2025-12-23 18:04:22 +0300

    update css reset file

--- components/shared/MobNavList.js
+++ components/shared/MobNavList.js
@@ -7,26 +7,26 @@ import speakers from 'public/images/shared/desktop/image-speakers.png';
 import earphones from 'public/images/shared/desktop/image-earphones.png';
 
 export default function MobNavList() {
-    const cards = [
-        { src: headphones, alt: 'headphones', h: headphones.height, w: headphones.width },
-        { src: speakers, alt: 'speakers', h: speakers.height, w: speakers.width },
-        { src: earphones, alt: 'earphones', h: earphones.height, w: earphones.width },
-    ];
-    return (
-        <ul className='MobNavList'>
-            {cards.map(card => (
-                <li key={uuid()} className='MobNavList__card'>
-                    <div className='MobNavList__img'>
-                        <Image src={card.src} alt={card.alt} layout='responsive' height={card.h} width={card.w} />
-                    </div>
-                    <span>{card.alt.toUpperCase()}</span>
-                    <Link href={`/${card.alt}`} legacyBehavior>
-                        <a aria-label={`Shop from the ${card.alt} department`}>
-                            SHOP <Image src={arrow} alt='arrow' />
-                        </a>
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    );
+  const cards = [
+    { src: headphones, alt: 'headphones', h: headphones.height, w: headphones.width },
+    { src: speakers, alt: 'speakers', h: speakers.height, w: speakers.width },
+    { src: earphones, alt: 'earphones', h: earphones.height, w: earphones.width },
+  ];
+  return (
+    <ul className='MobNavList'>
+      {cards.map(card => (
+        <li key={uuid()} className='MobNavList__card'>
+          <div className='MobNavList__img'>
+            <Image src={card.src} alt={card.alt} layout='responsive' height={card.h} width={card.w} />
+          </div>
+          <span>{card.alt.toUpperCase()}</span>
+          <Link href={`/${card.alt}`} legacyBehavior>
+            <a aria-label={`Shop from the ${card.alt} department`}>
+              SHOP <Image src={arrow} alt='arrow' />
+            </a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
